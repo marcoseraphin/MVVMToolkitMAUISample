e5ac8beaa449528562068ce7c64123fba3647dcf
Marco Seraphin <seraphin@develope-it.de>
2023-01-24 08:57:40 +0100

    added initial

---
+++ MVVMToolkitMAUISample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MVVMToolkitMAUISample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MVVMToolkitMAUISample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MVVMToolkitMAUISample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
